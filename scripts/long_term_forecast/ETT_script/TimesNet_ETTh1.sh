10204795f9b4508a221d0dc18ad74c5c5de39583
Zhaotianyou <zhaotianyou@mails.neu.edu.cn>
2026-03-16 13:49:25 +0000

    提交了参考论文和对应的sh代码启动文件

--- scripts/long_term_forecast/ETT_script/TimesNet_ETTh1.sh
+++ scripts/long_term_forecast/ETT_script/TimesNet_ETTh1.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES=2
+export CUDA_VISIBLE_DEVICES=0
 
 model_name=TimesNet
 
@@ -7,94 +7,20 @@ python -u run.py \
   --is_training 1 \
   --root_path ./dataset/ETT-small/ \
   --data_path ETTh1.csv \
-  --model_id ETTh1_96_96 \
+  --model_id ETTh1_OT_96_96 \
   --model $model_name \
   --data ETTh1 \
-  --features M \
+  --features S \
+  --target OT \
   --seq_len 96 \
   --label_len 48 \
   --pred_len 96 \
   --e_layers 2 \
   --d_layers 1 \
   --factor 3 \
-  --enc_in 7 \
-  --dec_in 7 \
-  --c_out 7 \
-  --d_model 16 \
-  --d_ff 32 \
-  --des 'Exp' \
-  --itr 1 \
-  --top_k 5 
-
-
-python -u run.py \
-  --task_name long_term_forecast \
-  --is_training 1 \
-  --root_path ./dataset/ETT-small/ \
-  --data_path ETTh1.csv \
-  --model_id ETTh1_96_192 \
-  --model $model_name \
-  --data ETTh1 \
-  --features M \
-  --seq_len 96 \
-  --label_len 48 \
-  --pred_len 192 \
-  --e_layers 2 \
-  --d_layers 1 \
-  --factor 3 \
-  --enc_in 7 \
-  --dec_in 7 \
-  --c_out 7 \
-  --d_model 16 \
-  --d_ff 32 \
-  --des 'Exp' \
-  --itr 1 \
-  --top_k 5
-
-
-python -u run.py \
-  --task_name long_term_forecast \
-  --is_training 1 \
-  --root_path ./dataset/ETT-small/ \
-  --data_path ETTh1.csv \
-  --model_id ETTh1_96_336 \
-  --model $model_name \
-  --data ETTh1 \
-  --features M \
-  --seq_len 96 \
-  --label_len 48 \
-  --pred_len 336 \
-  --e_layers 2 \
-  --d_layers 1 \
-  --factor 3 \
-  --enc_in 7 \
-  --dec_in 7 \
-  --c_out 7 \
-  --d_model 16 \
-  --d_ff 32 \
-  --des 'Exp' \
-  --itr 1 \
-  --top_k 5
-
-
-python -u run.py \
-  --task_name long_term_forecast \
-  --is_training 1 \
-  --root_path ./dataset/ETT-small/ \
-  --data_path ETTh1.csv \
-  --model_id ETTh1_96_720 \
-  --model $model_name \
-  --data ETTh1 \
-  --features M \
-  --seq_len 96 \
-  --label_len 48 \
-  --pred_len 720 \
-  --e_layers 2 \
-  --d_layers 1 \
-  --factor 3 \
-  --enc_in 7 \
-  --dec_in 7 \
-  --c_out 7 \
+  --enc_in 1 \
+  --dec_in 1 \
+  --c_out 1 \
   --d_model 16 \
   --d_ff 32 \
   --des 'Exp' \
